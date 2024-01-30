255ce1a167f7f17a11dadded572c8a1bf5e183d5
Pertsovych <vitalii.pertsovych@leoni.com>
2024-01-30 08:19:52 +0200

    Delete unnecessary description

---
+++ src/components/login.page.component.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+const LoginPage = () => {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container>
+        <h1>Hello I'm export</h1>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default LoginPage
