0b61312bc7a4f055cebc538216efa94477e56f1f
Pertsovych <vitalii.pertsovych@leoni.com>
2024-02-01 11:14:40 +0200

    Start with the main page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import LoginPage from './components/pages/login-page/login.page.component';
+import MainPage from './components/pages/main-page/main.page.component';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <header className="App-header">
         <HashRouter basename="/">
           <Routes>
-            <Route Component={LoginPage} exact path='/status'></Route>
+            <Route Component={MainPage} exact path='/main'></Route>
             <Route Component={LoginPage} exact path='/'></Route>
           </Routes>
         </HashRouter>
